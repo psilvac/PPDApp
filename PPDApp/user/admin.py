48a67d97988a45a1ab8ac2ce46bbf8dae712670f
Pablo <pablo.silvaco@alumnos.usm.cl>
2025-04-23 21:14:04 -0400

    cambios admin app user

--- PPDApp/user/admin.py
+++ PPDApp/user/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Permission
-
 from .models import Usuario
+from django.utils.translation import gettext_lazy as _
 
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Usuario._meta.fields]
+@admin.register(Usuario)
+class UsuarioAdmin(BaseUserAdmin):
+    model = Usuario
+    list_display = ('email', 'nombre', 'apellido', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    ordering = ('email',)
+    search_fields = ('email', 'nombre', 'apellido')
 
-admin.site.register(Usuario, UsuarioAdmin)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Información personal'), {'fields': ('nombre', 'apellido', 'clave_unica', 'organismo')}),
+        (_('Permisos'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Fechas importantes'), {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nombre', 'apellido', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
 admin.site.register(Permission)
