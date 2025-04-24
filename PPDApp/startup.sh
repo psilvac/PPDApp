141af479d8e304dcec2875a6fae53fa42a6f801c
Pablo <pablo.silvaco@alumnos.usm.cl>
2025-04-23 21:58:16 -0400

    user

--- PPDApp/startup.sh
+++ PPDApp/startup.sh
@@ -5,21 +5,5 @@ pip3 list
 pip3 install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py dbcheck
-
-echo "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(email='admin@example.com').exists():
-    User.objects.create_superuser(
-        email='admin@example.com',
-        password='admin123',
-        nombre='Admin',
-        apellido='User'
-    )
-    print('✔ Superusuario creado')
-else:
-    print('ℹ Superusuario ya existe')
-" | python manage.py shell
-
-
+python3 manage.py createsuperuser --noinput --nombre Jorge --apellido sanmartin
 python3 manage.py collectstatic && gunicorn --workers 2 PPDApp.wsgi
--- PPDApp/user/admin.py
+++ PPDApp/user/admin.py
@@ -1,29 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Permission
+
 from .models import Usuario
-from django.utils.translation import gettext_lazy as _
 
-@admin.register(Usuario)
-class UsuarioAdmin(BaseUserAdmin):
-    model = Usuario
-    list_display = ('email', 'nombre', 'apellido', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
-    ordering = ('email',)
-    search_fields = ('email', 'nombre', 'apellido')
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Usuario._meta.fields]
 
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Información personal'), {'fields': ('nombre', 'apellido', 'clave_unica', 'organismo')}),
-        (_('Permisos'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Fechas importantes'), {'fields': ('last_login',)}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'nombre', 'apellido', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
-    )
-
+admin.site.register(Usuario, UsuarioAdmin)
 admin.site.register(Permission)
