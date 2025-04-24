c38826fb9ea1fca4280f0f32ccff4a6c5ece71cd
Pablo <pablo.silvaco@alumnos.usm.cl>
2025-04-24 19:18:14 -0400

    cambio yaml

--- PPDApp/PPDApp/settings.py
+++ PPDApp/PPDApp/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'PPDApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Usa 'mysql' si estás usando MySQL
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
