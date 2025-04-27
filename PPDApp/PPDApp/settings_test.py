e945b47477f15e3ca406d612656ec3ecda5eb461
Pablo <pablo.silvaco@alumnos.usm.cl>
2025-04-27 00:49:31 -0400

    setting test

--- PPDApp/PPDApp/settings_test.py
+++ PPDApp/PPDApp/settings_test.py
@@ -6,11 +6,11 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'test_db'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': 'test_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
